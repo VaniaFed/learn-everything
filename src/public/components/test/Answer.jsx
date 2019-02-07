5342561bbf70df425673fbe9344b8ae74a895e3e
Ivan <vaniafed@nicePC.Dlink>
2019-02-07 19:21:24 +0300

    Answer, Test, TestItem

--- src/public/components/test/Answer.jsx
+++ src/public/components/test/Answer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
 
 const Answer = () => (
   <div className="item__bg">
-    <div className="item__title">Вопрос</div> 
+    <div className="item__title">Ответ</div> 
     <input type="text"
       className="test__input test__question"
-      disabled 
     />
   </div>
 );
--- src/public/components/test/Test.jsx
+++ src/public/components/test/Test.jsx
@@ -3,22 +3,30 @@ import '../../sass/index.sass';
 
 import React from 'react';
 import { render } from 'react-dom';
+
 import Title from './Title';
+import TestItem from './TestItem';
 
 const questionsList = [
   'Кто лох?',
   'Ты идиот?',
+  'Ты идиот?',
+  'Ты идиот?',
+  'Ты идиот?',
 ];
 
-const Test = ({ title }) => (
-  // <h1>hello</h1>
-  <Title title={title} />
-  // {questionsList.map((question, i) => 
-  //   <TestItem key={i} questionText={question}/>
-  // )};
+const Test = ({ title, questionsList }) => (
+  <div>
+    <Title title={title} />
+    <div className="test-items">
+      {questionsList.map((question, i) =>
+        <TestItem key={i} questionText={question}/>
+      )}
+    </div>
+  </div>
 );
 
 render(
-  <Test title="This is my title!"/>,
+  <Test title="This is my title!" questionsList={questionsList}/>,
   document.querySelector('#test')
 );
