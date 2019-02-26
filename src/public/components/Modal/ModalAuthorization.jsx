982c72e3f98f1ef022b59f6854a6cba155e6a799
Ivan <vaniafed@nicePC.Dlink>
2019-02-26 19:32:58 +0300

    add links and event onClick

--- src/public/components/Modal/ModalAuthorization.jsx
+++ src/public/components/Modal/ModalAuthorization.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 
-import Input from './Input';
 import Submit from './Submit';
 import Title from '../common/Title';
 
-const ModalAuthorization = ({ title }) => (
-  <div className="modal-bg">
-    <div className="modal-container">
-      <Title title={title} />
-      <Input className="modal__input modal__input__text" placeholder="Логин" />
-      <Input className="modal__input modal__input__text" placeholder="Пароль" />
-      <Submit className="modal__input modal__input__submit" value="Зарегистрироваться" />
-    </div>
-  </div>
-);
+class ModalAuthorization extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.userLogin = React.createRef();
+    this.userPassword = React.createRef();
+  }
+
+  login(e) {
+    e.preventDefault();
+    [this.userLogin.current.value, this.userPassword.current.value].map(i => console.log(i));
+  }
+
+  render () {
+    return (
+      <div className="modal-bg">
+        <div className="modal-container">
+          <Title title={this.props.title} />
+            <input className="modal__input modal__input__text" placeholder="Логин" ref={this.userLogin} />
+            <input className="modal__input modal__input__text" placeholder="Пароль" ref={this.userPassword} />
+          <Submit className="modal__input modal__input__submit" value="Войти" onClick={this.login}/>
+        </div>
+      </div>
+    )
+  }
+}
 
 ModalAuthorization.propTypes = {
   title: PropTypes.string,
