bedcbcea545c75cd65ad95e533ceb37ea1be38c4
VaniaFed <ztankist@gmail.com>
2019-03-21 12:00:29 +0300

    added constants and reducers

---
+++ src/public/constants.js
@@ -0,0 +1,5 @@
+const constants = {
+  ADD_DECK: 'ADD_DECK',
+};
+
+export default constants;
---
+++ src/public/reducers.js
@@ -0,0 +1,14 @@
+import C from './constants';
+
+
+export const decks = (state=[], action) => {
+  return [];
+}
+
+export const cards = (state=[], action) => {
+  return [];
+}
+
+export const card = (state={}, action) => {
+  return {};
+}
