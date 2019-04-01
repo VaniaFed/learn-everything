dafd9615e6f5bd3c2e1d505938049a363c070162
VaniaFed <ztankist@gmail.com>
2019-04-01 08:29:58 +0300

    deleted trash

--- src/public/Modal/ModalAuthorization.jsx
+++ src/public/Modal/ModalAuthorization.jsx
@@ -26,31 +26,23 @@ class ModalAuthorization extends Component {
 
     let error = ''
     let errorCode = 0
-
-    // if (!login.length) {
-    //   error = 'Вы не заполнили поле логин'
-    //   errorCode = 1
-    // } else if (!password.length) {
-    //   error = 'Вы не заполнили поле пароль'
-    //   errorCode = 2
-    // }
     (login.length === 0)
       ? error = 'Вы не заполнили поле логин'
       : password.length === 0
         ? error = 'Вы не заполнили поле пароль'
         : null
-    (error)
-        ? this.setState({
-          err: true,
-          errText: error,
-          errCode: errorCode,
-        }) :
-        this.setState({
-          err: false,
-          errText: '',
-          errCode: errorCode,
-        })
-    }
+        (error)
+          ? this.setState({
+            err: true,
+            errText: error,
+            errCode: errorCode
+          })
+          : this.setState({
+            err: false,
+            errText: '',
+            errCode: errorCode
+          })
+  }
 
   isError () {
     return this.state.err ? 'borderError' : ''
@@ -65,7 +57,7 @@ class ModalAuthorization extends Component {
             placeholder='Логин'
             ref={this.userLogin} />
           <input type='password' className='modal__input modal__input__text' placeholder='Пароль' ref={this.userPassword} />
-          <p style={{'color': 'red'}}>{this.state.err ? this.state.errText : null}</p>
+          <p style={{ 'color': 'red' }}>{this.state.err ? this.state.errText : null}</p>
           <Submit className='modal__input modal__input__submit' value='Войти' onClick={this.login} />
         </div>
       </div>
