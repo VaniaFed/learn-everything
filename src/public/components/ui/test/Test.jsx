0de21f9629b628b0d59c7630b3d76822a6be6218
VaniaFed <ztankist@gmail.com>
2019-04-02 12:15:36 +0300

    added opportunity to come back in test

--- src/public/components/ui/test/Test.jsx
+++ src/public/components/ui/test/Test.jsx
@@ -6,6 +6,18 @@ import Row from './row/Row'
 import Button from '../common/button/Button'
 
 import css from './test.module.sass'
+import NoOneCards from '../common/NoOneCards';
+
+const Rows = ({ cardsToPassTest }) => {
+  return (
+    cardsToPassTest.map((card, i) =>
+      <Row key={i}
+        questionText={card.question}
+        // onChange={(userAnswer) => changeCards (card.id, userAnswer)}
+      />
+    )
+  )
+}
 
 class Test extends Component {
   isAnswerCorrectly (correctAnswer, userAnswer) {
@@ -21,22 +33,24 @@ class Test extends Component {
   }
 
   render () {
-    const { cards, decks } = this.props
+    const { cards, decks, history } = this.props
     const { id } = this.props.match.params
     const currentDeck = decks.find(deck => deck.id === id)
-    const currentCards = cards.filter(card => card.deckId === id)
+    const cardsToPassTest = cards.filter(card => card.deckId === id)
     const { checkAllAnswers } = this
     return (
       <div className='container'>
         <Title className={css.title} title={currentDeck.title} />
         <div className={css.items}>
-          {currentCards.map((card, i) =>
-            <Row key={i}
-              questionText={card.question}
-              // onChange={(userAnswer) => changeCards (card.id, userAnswer)}
-            />
-          )}
-          <Button className='default-btn' content='Check the result' onClick={checkAllAnswers} />
+          {(cardsToPassTest.length > 0)
+            ? (
+              <div className="">
+                <Rows cardsToPassTest={cardsToPassTest} />
+                <Button className='default-btn' content='Check the result' onClick={checkAllAnswers} />
+              </div>
+            )
+            : <NoOneCards textMsg='Here is no one cards to pass the test' comeBack={history.goBack} />
+          }
         </div>
       </div>
     )
