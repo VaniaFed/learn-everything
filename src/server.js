84ab1bf60fdb6947bb4c74f6a31a55f8f6e247a1
VaniaFed <ztankist@gmail.com>
2019-04-08 11:28:19 +0300

    tried to add SSR

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import express from 'express'
 import { renderToString } from 'react-dom/server'
-import Cards from './public/components/containers/CardsList'
+import StyleContext from 'isomorphic-style-loader/StyleContext'
+import App from './public/components/App'
 
 global.React = React
-const html = renderToString(<Cards />)
 
 const logger = (req, res, next) => {
   console.log(`${req.method} request for ${req.url}`)
   next()
 }
 
-const sendHTMLPage = (req, res) => {
+const app = express()
+  .use(logger)
+
+app.get('*', (req, res) => {
+  const css = new Set() // CSS for all rendered React components
+  const insertCss = (...styles) => styles.forEach(style => css.add(style._getCss()))
+  const body = renderToString(
+    <StyleContext.Provider value={{ insertCss }}>
+      <App />
+    </StyleContext.Provider>
+  )
   res.status(200).send(`
     <!DOCTYPE html>
     <html lang="en">
@@ -19,14 +29,10 @@ const sendHTMLPage = (req, res) => {
       <title>Document</title>
     </head>
     <body>
-      <div id="root">${html}</div>
+      <div id="root">${body}</div>
     </body>
     </html>
   `)
-}
-
-const app = express()
-  .use(logger)
-  .use(sendHTMLPage)
+})
 
 app.listen(3000, () => console.log(`Server was running at 'http://localhost:3000'`))
