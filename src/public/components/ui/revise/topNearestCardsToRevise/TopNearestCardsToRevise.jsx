6d16dfc1d0a0122d06edc08cee6c1507de23363f
VaniaFed <ztankist@gmail.com>
2019-04-14 16:56:04 +0300

    quantity cards to show in top of nearest

--- src/public/components/ui/revise/topNearestCardsToRevise/TopNearestCardsToRevise.jsx
+++ src/public/components/ui/revise/topNearestCardsToRevise/TopNearestCardsToRevise.jsx
@@ -7,20 +7,53 @@ import { sortByDate } from '../../../../lib/time'
 export default class TopNearestCardsToRevise extends Component {
   constructor (props) {
     super(props)
+    this.sortedCards = props.cards.sort(sortByDate)
     this.state = {
       quantityCardsToShow: 5,
-      filteredCards: props.cards.sort(sortByDate)
+      filteredCards: this.sortedCards.slice(0, 5)
     }
-    console.log(this.state)
+    this.quantityCardsInput = React.createRef()
+    this.changeQuantityCardsToShow = this.changeQuantityCardsToShow.bind(this)
+  }
+
+  changeQuantityCardsToShow (number) {
+    let numberToState = this.isBeyondTheRangeFixIt(number)
+    this.setState({
+      quantityCardsToShow: numberToState,
+      filteredCards: this.sortedCards.slice(0, numberToState)
+    })
+  }
+
+  isBeyondTheRangeFixIt (inputNumber) {
+    let resultNumber = inputNumber
+
+    if (resultNumber < 0) {
+      resultNumber = 0
+    }
+    if (resultNumber > 500) {
+      resultNumber = 500
+    }
+
+    return resultNumber
   }
 
   render () {
+    const { quantityCardsInput, changeQuantityCardsToShow } = this
+    const { quantityCardsToShow, filteredCards } = this.state
     return (
       <div className={css.container}>
         <Title2 content='Ближайшие карточки для повторения' />
-        <label className={css.quantity_label} htmlFor='quantity_cards'>Количество карточек</label>
-        <input className={css.quantity_input} id='quantity_cards' type='number' min={0} max={500} placeholder='0-500' defaultValue={5} />
-        <CardsContainer cards={this.state.filteredCards} />
+        <label className={css.quantity_label} htmlFor='quantity_cards'>Количество карточек:</label>
+        <input className={css.quantity_input}
+          id='quantity_cards'
+          ref={quantityCardsInput}
+          type='number'
+          onChange={() => changeQuantityCardsToShow(+quantityCardsInput.current.value)}
+          min={0}
+          max={500}
+          placeholder='0-500'
+          defaultValue={5} />
+        <CardsContainer cards={filteredCards} quantityCardsToShow={quantityCardsToShow} />
       </div>
     )
   }
