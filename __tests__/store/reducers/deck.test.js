c5699ce04468247f5b9f1d46e571401ae9f454d3
VaniaFed <ztankist@gmail.com>
2019-04-18 14:24:43 +0300

    wrote all the tests for deck Reducer

---
+++ __tests__/store/reducers/deck.test.js
@@ -0,0 +1,42 @@
+import C from '../../../src/public/constants'
+import deepFreeze from 'deep-freeze'
+import { deck } from '../../../src/public/store/reducers'
+
+describe('Deck Reducer', () => {
+  it(`${C.ADD_DECK} success`, () => {
+    const state = {}
+    const action = {
+      type: C.ADD_DECK,
+      id: 0,
+      title: 'First deck'
+    }
+    deepFreeze(state)
+    deepFreeze(action)
+    const result = deck(state, action)
+    expect(result)
+      .toEqual({
+        id: 0,
+        title: 'First deck'
+      })
+  })
+
+  it(`${C.REMOVE_DECK} success`, () => {
+    const state = {
+      id: 0,
+      title: 'Some deck'
+    }
+    const action = {
+      type: C.RENAME_DECK,
+      id: 0,
+      title: 'New deck name'
+    }
+    deepFreeze(state)
+    deepFreeze(action)
+    const result = deck(state, action)
+    expect(result)
+      .toEqual({
+        id: 0,
+        title: 'New deck name'
+      }) 
+  })
+})
