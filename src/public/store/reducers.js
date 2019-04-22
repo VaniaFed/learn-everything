50eef2a870af39ed9d01b83fa1857bb310327687
VaniaFed <ztankist@gmail.com>
2019-04-22 18:07:25 +0300

    improved

--- src/public/store/reducers.js
+++ src/public/store/reducers.js
@@ -24,7 +24,7 @@ export const card = (state = {}, action) => {
         answer,
         datePrevRevise,
         dateNextRevise,
-        deckId: deckId
+        deckId
       }
     }
     case C.CHANGE_QUESTION: {
