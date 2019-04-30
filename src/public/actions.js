7a2e4396a7d1c6e571518dd780a40f744797a303
VaniaFed <ztankist@gmail.com>
2019-04-30 22:48:13 +0300

    fixed bug

--- src/public/actions.js
+++ src/public/actions.js
@@ -21,7 +21,7 @@ export const addCard = (newDeckId, cardId, question, answer) => {
   return (
     {
       type: C.ADD_CARD,
-      id: (typeof cardId !== undefined) ? cardId : v4(),
+      id: (typeof cardId !== 'undefined') ? cardId : v4(),
       question: question || '',
       answer: answer || '',
       datePrevRevise: currentDate,
