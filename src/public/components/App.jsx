8cb21d784c624f8e62de0a2ebf94fb4790d84b97
VaniaFed <ztankist@gmail.com>
2019-05-06 13:27:25 +0300

    some improvements

--- src/public/components/App.jsx
+++ src/public/components/App.jsx
@@ -23,11 +23,9 @@ import ModalRegistration from './ui/Modal/ModalRegistration'
     15. Cards: поиск по картачкам
     16. Cards: sort by date revise (by default)
     17. React tests
-    18. Redux tests
     19. Зарефакторить modals: вынести общий код в контейнер и не только :)
     20. Изучить и добавить HOC везде, где это необходимо
     21. Make code schem: with the help of UML diagrams
-    22. Пофиксить кнопку comeBack in Test
 */
 class App extends Component {
   render () {
