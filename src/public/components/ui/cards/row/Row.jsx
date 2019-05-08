9e8204c3f412c46b0746529545640dfc1e37fde3
VaniaFed <ztankist@gmail.com>
2019-05-08 11:04:41 +0300

    some rafactors

--- src/public/components/ui/cards/row/Row.jsx
+++ src/public/components/ui/cards/row/Row.jsx
@@ -20,7 +20,7 @@ class Row extends Component {
       onChangeAnswer = f => f
     } = this.props
     return (
-      <div className={css.item} style={this.style}>
+      <div className={css.item}>
         <svg className={css.delete} onClick={onDelete} viewBox='0 0 64 64' width='15px'>
           <path fill='#1D1D1B' d='M29 31.8L.5 60A2 2 0 1 0 3.5 63L32 34.4 60.5 63a2 2 0 0 0 2.9 0c.8-.8.8-2.1 0-2.9L35 31.8 63.4 3.4A2 2 0 1 0 60.6.6L32 29 3.4.6A2 2 0 0 0 .6 3.4l28.3 28.4z' />
         </svg>
